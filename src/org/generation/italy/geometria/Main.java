5d98b87a4b200a25c97b568c60702cc3b7635455
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-11-23 17:02:15 +0100

    completato

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -7,14 +7,17 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner (System.in);
-		
-		
 		Rettangolo rettangolo1 = new Rettangolo();
+		
+		
+		System.out.print("Base : ");
 		rettangolo1.altezza = scan.nextInt();
+		System.out.print("Altezza : ");
 		rettangolo1.base = scan.nextInt();
 
-		System.out.println(rettangolo1.calcolaArea());
-		System.out.println(rettangolo1.calcolaPerimetro());
+		
+		System.out.println("Perimetro : " + rettangolo1.calcolaPerimetro());
+		System.out.println("Area : " + rettangolo1.calcolaArea());
 		
 		
 		
